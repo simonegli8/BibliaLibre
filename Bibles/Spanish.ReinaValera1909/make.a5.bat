a6069953aacc699faee6ea867c3cf52737544998
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-03-06 06:41:18 +0100

    Added A5 RVR1909

--- Bibles/Spanish.ReinaValera1909/make.a5.bat
+++ Bibles/Spanish.ReinaValera1909/make.a5.bat
@@ -7,5 +7,6 @@ xelatex Biblia10ptA5 -output-directory=..\out\pdf
 
 cd ..\out\pdf
 move Biblia10ptA5.pdf ReinaValera1909.10pt.A5.pdf
+impose -l folio -M -m 0 -o landscape --forms 16 -d A4 ReinaValera1909.10pt.A5.pdf
 
 cd ..\..\
